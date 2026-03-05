f84dec28d3a2e50d021732771537472f0afbdee9
Thuat Nguyen <nguyenthienthuat992@gmail.com>
2026-03-05 15:56:16 +0700

    feat: implement preprocessing class with fit_transform and transform methods

--- bfmdt/preprocessing.py
+++ bfmdt/preprocessing.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-
 class Preprocessor:
     """Clean and normalize raw data for the pipeline.
 
@@ -16,11 +15,49 @@ class Preprocessor:
     def __init__(self):
         self.feature_min_ = None
         self.feature_max_ = None
+        self.feature_mean_ = None
+        self.valid_features_mask_ = None
 
     def fit_transform(self, X, y):
-        # TODO: implement
-        raise NotImplementedError
+        valid_y_mask = ~np.isnan(y)
+        X_clean = X[valid_y_mask].copy()
+        y_clean = y[valid_y_mask].copy()
+
+        self.feature_mean_ = np.nanmean(X_clean, axis=0)
+        self.feature_mean_[np.isnan(self.feature_mean_)] = 0.0 
+        
+        nan_mask = np.isnan(X_clean)
+        # for i in range(X_clean.shape[1]):
+        #     X_clean[nan_mask[:, i], i] = self.feature_mean_[i]
+        X_clean = np.where(nan_mask, self.feature_mean_, X_clean)
+
+        self.feature_min_ = np.min(X_clean, axis=0)
+        self.feature_max_ = np.max(X_clean, axis=0)
+        feature_range = self.feature_max_ - self.feature_min_
+        
+        self.valid_features_mask_ = feature_range > 0
+        
+        X_clean = X_clean[:, self.valid_features_mask_]
+        self.feature_min_ = self.feature_min_[self.valid_features_mask_]
+        feature_range = feature_range[self.valid_features_mask_]
+        
+        X_clean = (X_clean - self.feature_min_) / feature_range
+        
+        return X_clean, y_clean
 
     def transform(self, X):
-        # TODO: implement
-        raise NotImplementedError
+        X_clean = X.copy()
+        
+        nan_mask = np.isnan(X_clean)
+        # for i in range(X_clean.shape[1]):
+        #     X_clean[nan_mask[:, i], i] = self.feature_mean_[i]
+        X_clean = np.where(nan_mask, self.feature_mean_, X_clean)
+        
+        X_clean = X_clean[:, self.valid_features_mask_]
+        
+        feature_range = self.feature_max_[self.valid_features_mask_] - self.feature_min_
+        X_clean = (X_clean - self.feature_min_) / feature_range
+        
+        X_clean = np.clip(X_clean, 0.0, 1.0)
+        
+        return X_clean
